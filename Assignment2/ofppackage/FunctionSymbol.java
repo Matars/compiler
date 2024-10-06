880a7aebfb06bda41e16fecf1c138204b1e39d50
km222uq <km222uq@student.lnu.se>
2024-10-06 14:52:45 +0200

    still fixing git ignore

---
+++ Assignment2/ofppackage/FunctionSymbol.java
@@ -0,0 +1,38 @@
+package ofppackage;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class FunctionSymbol extends Symbol {
+    private List<Symbol> parameters;
+
+    public FunctionSymbol(OFPType type, String name) {
+        super(type, name);
+        this.parameters = new ArrayList<>();
+    }
+
+    public void addParameter(Symbol param) {
+        parameters.add(param);
+    }
+
+    public int getParameterCount() {
+        return parameters.size();
+    }
+
+    public List<Symbol> getParameters() {
+        return parameters;
+    }
+
+    public List<OFPType> getParameterTypes() {
+        List<OFPType> types = new ArrayList<>();
+        for (Symbol param : parameters) {
+            types.add(param.getType());
+        }
+        return types;
+    }
+
+    public Symbol getParameter(int index) {
+        return parameters.get(index);
+    }
+
+}
